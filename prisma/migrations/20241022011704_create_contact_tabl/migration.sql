27fadf2f6595e25b5485c790ad2e4b776beb43c0
ALAN RUFFO <alanruffo777@gmail.com>
2024-10-21 22:17:37 -0300

    migrations

--- prisma/migrations/20241022011704_create_contact_tabl/migration.sql
+++ prisma/migrations/20241022011704_create_contact_tabl/migration.sql
@@ -1,16 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `contatos` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `items` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `contatos`;
-
--- DropTable
-DROP TABLE `items`;
-
 -- CreateTable
 CREATE TABLE `Contato` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
